89455aa89aa478cc234e29831130c52f93473367
xiaoluo <lpyuan21@outlook.com>
2021-06-27 19:23:02 +0800

    更新代码

--- libpcap/traffic_analysis.h
+++ libpcap/traffic_analysis.h
@@ -8,10 +8,11 @@
 #ifndef HERA_CODE_TRAFFIC_ANALYSIS_H
 #define HERA_CODE_TRAFFIC_ANALYSIS_H
 
+#if 0
 typedef struct _eth_hdr{
     unsigned char dstmac[6];    //目的mac地址
     unsigned char srcmac[6];    //源mac地址
     unsigned short eth_type;    //以太网类型
 }eth_hdr;
-
+#endif
 #endif
