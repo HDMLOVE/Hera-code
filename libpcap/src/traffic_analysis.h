c38e07dd9a5871500cb6721e3b828387a6812ffc
xiaoluo <lpyuan21@outlook.com>
2021-06-27 22:44:17 +0800

    大小端问题导致数据解析错误

--- libpcap/src/traffic_analysis.h
+++ libpcap/src/traffic_analysis.h
@@ -12,6 +12,13 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+// x86是小端存储
+#ifndef __LITTLE_ENDIAN_BITFIELD
+#define __LITTLE_ENDIAN_BITFIELD (1)
+#else
+#define __LITTLE_ENDIAN_BITFIELD
+#endif
+
 //Mac头部，总长度14字节
 typedef struct _eth_hdr
 {
@@ -36,8 +43,13 @@ typedef struct _eth_hdr
 //IP头部，总长度20字节
 typedef struct _ip_hdr
 {
+#ifdef __LITTLE_ENDIAN_BITFIELD
+    unsigned char ihl:4;
+    unsigned char version:4
+#else
     unsigned char version:4;    //版本
 	unsigned char ihl:4;        //首部长度
+#elif
     unsigned char tos;          //服务类型
     unsigned short tot_len;     //总长度
     unsigned short id:3;        //标志
@@ -56,12 +68,17 @@ typedef struct _tcp_hdr
     unsigned short dst_port;    //目的端口号
     unsigned int seq_no;        //序列号
     unsigned int ack_no;        //确认号
-
+#ifdef __LITTLE_ENDIAN_BITFIELD
+    unsigned char reserved_1:4; //保留6位中的4位首部长度
+    unsigned char thl:4;        //tcp头部长度
+    unsigned char flag:6;       //6位标志
+	unsigned char reseverd_2:2; //保留6位中的2位
+#else
     unsigned char thl:4;        //tcp头部长度
 	unsigned char reserved_1:4; //保留6位中的4位首部长度
 	unsigned char reseverd_2:2; //保留6位中的2位
-	unsigned char flag : 6;     //6位标志
-
+	unsigned char flag:6;       //6位标志
+#endif
     unsigned short wnd_size;    //16位窗口大小
     unsigned short chk_sum;     //16位TCP检验和
     unsigned short urgt_p;      //16为紧急指针
