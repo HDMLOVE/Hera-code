580b1c218734210edac6c820b69643714a0a10d5
HDMLOVE <lpyuan21@outlook.com>
2021-06-27 19:38:36 +0800

    上传代码

---
+++ libpcap/src/traffic_analysis.h
@@ -0,0 +1,18 @@
+/******************************************
+ * Filename : traffic_analysis.h
+ * Time     : 2021-06-27 12:00
+ * Author   : 小骆
+ * Dcription: 
+*******************************************/
+
+#ifndef HERA_CODE_TRAFFIC_ANALYSIS_H
+#define HERA_CODE_TRAFFIC_ANALYSIS_H
+
+#if 0
+typedef struct _eth_hdr{
+    unsigned char dstmac[6];    //目的mac地址
+    unsigned char srcmac[6];    //源mac地址
+    unsigned short eth_type;    //以太网类型
+}eth_hdr;
+#endif
+#endif
